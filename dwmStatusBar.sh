9fc1695e8287807a83ac4197614d190d00f13de7
Dan2796 <danjsnow@outlook.com>
2020-07-20 08:15:47 +0100

    Bit more on the status bar for battery etc

--- dwmStatusBar.sh
+++ dwmStatusBar.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+SOUND=$(pactl list sinks | grep 'Volume: f' | grep -o '[0-9]\+%' | head -1)
+
 BATPERC=$(upower -i $(upower -e | grep 'BAT') | grep -E "percentage" | xargs echo)
 
 BATSTATE=$(upower -i $(upower -e | grep 'BAT') | grep -E "state|to\ full" | xargs echo)
 
 DATE=$(date +"%a, %d %b, %H:%M")
 
-xsetroot -name "$BATPERC $BATSTATE | $DATE"
+xsetroot -name "sound: $SOUND | $BATPERC $BATSTATE | $DATE"

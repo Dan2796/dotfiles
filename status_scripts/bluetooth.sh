40bba026d504f3a64464a7a847c7a4dccca1d0f6
Dan Snow <danjsnow@outlook.com>
2021-06-05 11:41:25 +0100

    Added dwm script for bluetooth, vim goyo gy rnu added

--- status_scripts/bluetooth.sh
+++ status_scripts/bluetooth.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-bluetoothctl info | grep Alias | sed s/\ //g | sed 's/^[ \t]*//' | sed s/Alias://g
+CONNECTED=$(bluetoothctl info | grep Alias | sed s/\ //g | sed 's/^[ \t]*//' | sed s/Alias://g)
 
+echo  $CONNECTED

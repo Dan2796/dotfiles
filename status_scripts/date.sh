bd9efea43fff33d22cf4b1b1a6eba6a63d3de9b9
Dan Snow <danjsnow@outlook.com>
2022-06-01 09:19:12 +0100

    Updated date to europe time

--- status_scripts/date.sh
+++ status_scripts/date.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 LHS_ARROW="^b#2e3440^^c#e5e9f0^"
-DATE=$(date +"%a, %d %b, %H:%M")
+
+# europe time. manual adjust because it's easier
+DATE=$(date -d "+ 1 hour" +"%a, %d %b, %H:%M")
+#DATE=$(date +"%a, %d %b, %H:%M")
 
 echo "$LHS_ARROW   $DATE "

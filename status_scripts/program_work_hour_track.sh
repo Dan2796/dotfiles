a247345a2b804365e09a97cfd3f9c6695e8a9c26
Dan Snow <danjsnow@outlook.com>
2022-08-17 16:18:44 +0100

    Added programming timer

--- status_scripts/program_work_hour_track.sh
+++ status_scripts/program_work_hour_track.sh
@@ -4,12 +4,12 @@ source /home/dan/dotfiles/status_scripts/nord_colour_scheme.sh
 
 # Arrow is given in the timetrace one so it can change colour
 lhs_arrow="^b"$polar_4"^^c"$snow_1"^"
-purple_lhs_arrow="^c"$aurora_5"^^b"$aurora_5"^^c"$polar_4"^"
-from_teal_lhs_arrow="^b"$frost_3"^^c"$polar_4"^^c"$snow_1"^^b"$polar_4"^"
+teal_lhs_arrow="^c"$frost_3"^^b"$frost_3"^^c"$polar_4"^"
+from_red_lhs_arrow="^b"$aurora_1"^^c"$polar_4"^^c"$snow_1"^^b"$polar_4"^"
 
 # note that it always rounds down - easier than trying to round for 
 # individual seconds...
-deep_time=$(timew summary today deep | tail -n 2 | xargs | awk -F ":" '{print $1 "h" $2 "m"}')
+program_time=$(timew summary today programming | tail -n 2 | xargs | awk -F ":" '{print $1 "h" $2 "m"}')
 
 # correct to zero if it throws the no filtered data message
 if [[ $deep_time == *"filtered"* ]]; then
@@ -20,15 +20,15 @@ fi
 if [[ -f "/home/dan/dotfiles/status_scripts/status_files/chillin.txt" ]]; then
   output=""
 elif [[ $(timew | head -1 | grep -o "programming") == "programming" ]]; then
-  output="$from_teal_lhs_arrow "D:"$deep_time"
+  output="$teal_lhs_arrow "P:"$program_time"
 elif [[ $(timew | head -1 | grep -o "deep") == "deep" ]]; then
-  output="$purple_lhs_arrow "D:"$deep_time"
+  output="$lhs_arrow "P:"$program_time"
 elif [[ $(timew | head -1 | grep -o "shallow") == "shallow" ]]; then
-  output="$lhs_arrow "D:"$deep_time"
+  output="$lhs_arrow "P:"$program_time"
 elif [[ -f "/home/dan/dotfiles/status_scripts/status_files/workin.txt" ]]; then
-  output="$lhs_arrow "D:"$deep_time"
+  output="$from_red_lhs_arrow "P:"$program_time"
 else
-  output="$lhs_arrow "D:"$deep_time"
+  output="$lhs_arrow "P:"$program_time"
 fi
 
 echo $output

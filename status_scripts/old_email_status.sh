db2b9c09b32a09d8900d1f588c068d81642cab52
Dan Snow <danjsnow@outlook.com>
2023-04-24 13:24:07 +0100

    Now no email status because mutt not working

---
+++ status_scripts/old_email_status.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+source /home/dan/dotfiles/status_scripts/nord_colour_scheme.sh
+
+lhs_arrow="^c"$polar_3"^^c"$snow_1"^^b"$polar_3"^"
+
+# Show syncing time
+last_checked=$(cat ~/dotfiles/status_scripts/status_files/email_last_synced.txt)
+last_checked_seconds=$(($(date +%s) - $last_checked))
+last_checked_ago=$(($(($(date +%s) - $last_checked))/60))
+
+email_numbers=$(cat ~/dotfiles/status_scripts/status_files/email_number.txt)
+if [[ -f "/home/dan/dotfiles/status_scripts/status_files/email_syncing.txt" ]]; then
+  email_sync="syncing "
+else
+  email_sync=""
+fi
+
+if (( last_checked_seconds <= 10 )); then
+  long_ago_message="just now"
+elif (( last_checked_seconds <= 120 )); then
+  long_ago_message="1 min"
+else
+  long_ago_message="$last_checked_ago mins"
+fi
+
+if [[ -f "/home/dan/dotfiles/status_scripts/status_files/workin.txt" ]]; then
+  output=""
+elif [[ -f "/home/dan/dotfiles/status_scripts/status_files/chillin.txt" ]]; then
+  output=""
+else 
+  output="$lhs_arrow $email_sync$email_numbers   $long_ago_message"
+fi
+
+echo $output
+

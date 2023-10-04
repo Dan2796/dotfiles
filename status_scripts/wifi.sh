6f40adb432b7ff04f2a25bfc82a6073b5c956a10
Dan Snow <danjsnow@outlook.com>
2023-10-04 14:26:37 +0100

    Creates slimmed version of dotfiles
    Delete loads of bits that aren't useful on my work computer

--- status_scripts/wifi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-source /home/dan/dotfiles/status_scripts/nord_colour_scheme.sh
-
-lhs_arrow="^b"$polar_3"^^c"$snow_1"^"
-
-wifi="$(nmcli | grep -o 'connected to .\+' | sed 's/connected to / /g')"
-
-echo "$lhs_arrow $wifi"
--- symbolic_link.sh
+++ symbolic_link.sh
@@ -13,40 +13,9 @@ ln -sf ~/dotfiles/zsh/zshenv ~/.zshenv
 ln -sf ~/dotfiles/R/Rprofile ~/.Rprofile
 ln -sf ~/dotfiles/R/Renviron ~/.Renviron
 
-# Picom
-ln -sf ~/dotfiles/picom.conf ~/.config/picom.conf 
-
-# Newsboat
-ln -sf ~/dotfiles/newsboat/config ~/.newsboat/config
-ln -sf ~/dotfiles/newsboat/urls ~/.newsboat/urls
-
-# Xprofile
-ln -sf ~/dotfiles/xprofile ~/.xprofile
-
-# Xmodmap
-ln -sf ~/dotfiles/xmod/Xmodmap ~/.Xmodmap
-
 # Zathura
 ln -sf ~/dotfiles/zathurarc ~/.config/zathura/zathurarc
-
-# Ranger
-ln -sf ~/dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf
-ln -sf ~/dotfiles/ranger/rifle.conf ~/.config/ranger/rifle.conf
 
 # Tmux
 ln -sf ~/dotfiles/tmux.conf ~/.config/tmux/tmux.conf
 
-# Scli
-ln -sf ~/dotfiles/sclirc ~/.config/sclirc
-
-# Dunst
-ln -sf ~/dotfiles/dunstrc ~/.config/dunst/dunstrc
-
-# Spotify
-ln -sf ~/dotfiles/spotifyd.conf ~/.config/spotifyd/spotifyd.conf
-
-# SSH
-ln -sf ~/dotfiles/ssh_config ~/.ssh/config
-
-# Calcurse
-ln -sf ~/dotfiles/calcurse/ ~/.config/calcurse
